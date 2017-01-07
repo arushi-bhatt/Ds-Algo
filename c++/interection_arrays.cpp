7155f8a57b894e53a8133066751a57cd7fea12a2
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-07 23:13:50 +0530

    new program intersection of array added and in pair sum program , UI is increased

---
+++ c++/interection_arrays.cpp
@@ -0,0 +1,46 @@
+//INTERSECTION OF ARRAYS (unsorted)
+
+#include<iostream>
+using namespace std;
+
+void intersectionOfArray(int a[],int b[],int na, int nb){
+    int i,j;
+
+    int *done = new int[nb];
+    for(i=0;i<nb;i++){
+        done[i]=0;
+    }
+
+    for(i=0;i<na;i++){
+        for(j=0;j<nb;j++){
+            if(!done[j])
+            if(a[i]==b[j]){
+                cout<<a[i]<<",";
+                done[j]++;
+                break;
+            }
+        }
+    }
+delete [] done;
+}
+
+int main()
+{
+    int a[20],b[20],na,nb;
+
+cout<<"no. of elements in array 1 and array 2: ";
+cin>>na>>nb;
+cout<<endl;
+int j,i;
+cout<<"Enter elements of array 1: ";
+for(i=0;i<na;i++)
+cin>>a[i];
+
+cout<<"Enter elements of array 2: ";
+for(i=0;i<nb;i++)
+cin>>b[i];
+
+intersectionOfArray(a,b,na,nb);
+cout<<"Intersection of array : "<<endl;
+return 0;
+}
--- c++/pairSum.cpp
+++ c++/pairSum.cpp
@@ -60,7 +60,7 @@ void bubbleSort(int arr[],int n){
 //version 3 {working correctly}
 void pairSum(int arr[], int n, int sum)
 {
-     // Initialize result
+     //to print pairs in ascending order
      bubbleSort(arr,n);
 
     // Consider all possible pairs and check their sums
@@ -71,9 +71,20 @@ void pairSum(int arr[], int n, int sum)
 
 }
  int main(){
-     int input[] = {1,1,1,1};
-     pairSum(input,4,2);
-     return 0;
+     int n,sum;
+     cout<<"no. of elements in array and value of sum :: ";
+    cin>>n>>sum;
+    cout<<endl;
+
+    int i;
+
+    int *a=new int[n];
+    cout<<"Enter elements of array: ";
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    cout<<endl<<"pairs: "<<endl;
+    pairSum(a,n,sum);
+    return 0;
  }
 
 
