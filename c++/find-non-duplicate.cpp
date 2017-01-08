f95564fba70721c39a9e2dbddb2995dc20d8590f
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-08 12:25:57 +0530

    Given 2N+1 numbers out of which N are duplicate find the non-duplicate

---
+++ c++/find-non-duplicate.cpp
@@ -0,0 +1,51 @@
+
+// Given 2N+1 numbers out of which N are duplicate find the non-duplicate number.
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int arr[20],n;
+    cout<<"no. of elements (odd no.): ";
+    cin>>n;
+    cout<<endl;
+    cout<<"Enter elements (only on of them is non-duplicate): ";
+    int i;
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+
+    long int x=0;
+    for(i=0;i<n;i++)
+           x=x^arr[i];
+
+    cout<<x;
+        cout<<endl;
+        return 0;
+}
+
+/*
+//convert decimal into binary
+int division(long int num,int base)
+{
+    if (num/base==0)
+        return 1;
+    else
+        return((num%base)+division(num/base,base)*10);
+}
+//converting binary no. to decimal
+int basepower(long int  num,int base)    //*(base)^n
+{
+    int i =0,res=0;
+    long int  q;
+    q=num;
+while(q>0)
+{
+    res+=(q%10)*(pow(base,i));
+    q=q/10;
+    i++;
+}
+    return res;
+}
+*/
