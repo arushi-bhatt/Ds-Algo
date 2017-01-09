83e239fbd8fe1a1567dc4e602967f9fb5d8c760a
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-10 01:01:52 +0530

    bug fixed in sort '0 1 2' program, indentation checked in sort01 program

--- c++/sort 0 1 2array.cpp
+++ c++/sort 0 1 2array.cpp
@@ -4,36 +4,54 @@
 #include<cmath>
 using namespace std;
 
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+void sort012(int arr[], int n)  {
 
+	// Write your code here
+    int i,j,k;
+
+    //initialize i and k with starting index and j with last index
+    // before i , all elements are zero
+    //after j ,all elements are 2
+    //k will represent the current element
+    //important : k=1 initially
+
+    for(i=0,j=n-1,k=1;k<j;)
+    {
+        cout<<i<<j<<k<<endl;
+
+        if(arr[k]==0)
+            {swap(arr[i],arr[k]);
+             i++;}
+
+        else if(arr[k]==2)
+            {swap(arr[k],arr[j]);
+            j--;}
+
+        else  k++;
+    }
+}
 int main()
 {
     int arr[20],n;
-cout<<"no. of elements : ";
-cin>>n;
-cout<<endl;
-cout<<"Enter elements : ";
-int i,j,k;
-for(i=0;i<n;i++)
-cin>>arr[i];
+    cout<<"no. of elements : ";
+    cin>>n;
+    cout<<endl;
 
+    cout<<"Enter elements : ";
+    int i;
+    for(i=0;i<n;i++)
+        cin>>arr[i];
 
+    sort012(arr,n);
 
-for(i=0,j=n-1,k=0;k<=j;)
-{
-
-    if(arr[k]==0){swap(arr[i],arr[k]);
-    i++; k++;}
-else if(arr[k]==2){
-        swap(arr[k],arr[j]);
-    j--;}
-   else  k++;
-}
-cout<<endl<<"sorted array : "<<endl;
-for(i=0;i<n;i++)
-{
-    cout<<arr[i]<<" ";
-}
-
+    cout<<endl<<"sorted array : "<<endl;
+    for(i=0;i<n;i++)
+        cout<<arr[i]<<" ";
     cout<<endl;
     return 0;
 }
--- c++/sort 0 1 array.cpp
+++ c++/sort 0 1 array.cpp
@@ -5,41 +5,49 @@ sort this array. Do it in one pass, this means you can’t first find the count 
 #include<cmath>
 using namespace std;
 
+// arr - input array
+// n - size of array
+void SortZeroesAndOne(int arr[], int n){
+    int i,j;
+    for(i=0,j=n-1;!(i>j);)
+    {
+        if(arr[i]==1&&arr[j]==0&& i!=j)
+            {
+                arr[i]=0;
+                arr[j]=1;
+
+            }
+        else {if(arr[i]==0)
+                i++;
+
+              if(arr[j]==1)
+                j--;
+             }
+    }
+}
 
 int main()
 {
     int arr[20],n;
-cout<<"no. of elements : ";
-cin>>n;
-cout<<endl;
-cout<<"Enter elements : ";
-int i,j;
-for(i=0;i<n;i++)
-cin>>arr[i];
+    cout<<"no. of elements : ";
+    cin>>n;
+    cout<<endl;
+    cout<<"Enter elements : ";
+    int i,j;
 
-int temp;
-for(i=0,j=n-1;!(i>j);)
-{
- if(arr[i]==1&&arr[j]==0&& i!=j)
-    {
-        arr[i]=0;
-        arr[j]=1;
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<endl;
 
-    }
-    else {if(arr[i]==0)
-        i++;
-        cout<<i;
+    SortZeroesAndOne(arr,n);
+    //print the elements
+    for(i=0;i<n;i++)
+        cout<<arr[i]<<" " ;
 
-    if(arr[j]==1)
-        j--;
-        cout<<j;
-    }
-    }
-cout<<endl<<"sorted array : "<<endl;
-for(i=0;i<n;i++)
-{
-    cout<<arr[i]<<" ";
-}
+
+
+
+
 
     cout<<endl;
     return 0;
