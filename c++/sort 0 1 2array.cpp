2d684bcab82024ca641c17ba5430869c66d24ea4
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-09 23:46:31 +0530

    sort elements of '0,1,2' program added

---
+++ c++/sort 0 1 2array.cpp
@@ -0,0 +1,40 @@
+
+// assignment 4 ques 2
+//find pair which make sum of x
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    int arr[20],n;
+cout<<"no. of elements : ";
+cin>>n;
+cout<<endl;
+cout<<"Enter elements : ";
+int i,j,k;
+for(i=0;i<n;i++)
+cin>>arr[i];
+
+
+
+for(i=0,j=n-1,k=0;k<=j;)
+{
+
+    if(arr[k]==0){swap(arr[i],arr[k]);
+    i++; k++;}
+else if(arr[k]==2){
+        swap(arr[k],arr[j]);
+    j--;}
+   else  k++;
+}
+cout<<endl<<"sorted array : "<<endl;
+for(i=0;i<n;i++)
+{
+    cout<<arr[i]<<" ";
+}
+
+    cout<<endl;
+    return 0;
+}
