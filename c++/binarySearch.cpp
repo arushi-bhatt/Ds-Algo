a66067e5930bf9907db3cf37a0231bf57b9a1618
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-11 00:08:12 +0530

    binary search program completed

--- c++/binarySearch.cpp
+++ c++/binarySearch.cpp
@@ -1,5 +1,9 @@
+//Binary Search (only for sorted array)
 
-int BinarySearchHelper(int arr[], int l, int r, int x)
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
 {
     if (r >= l)
     {
@@ -18,4 +22,27 @@ int BinarySearchHelper(int arr[], int l, int r, int x)
 
     // We reach here when element is not present in array
     return -1;
+}
+
+
+int main()
+{
+    int n;
+    cout<<"no. of elements : ";
+    cin>>n;
+
+    cout<<endl;
+
+    int *arr=new int[n];//declare dynamic array
+
+    cout<<"Enter elements : ";
+    int i;
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    int no;
+    cout<<"NO. U want to search : ";
+    cin>>no;
+    cout<<"position (=-1 if not found ): "<< binarySearch(arr,0,n-1,no)+1;
+    delete [] arr;
+    return 0;
 }
