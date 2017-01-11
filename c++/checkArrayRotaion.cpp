48e125a1af4ce76e9a8a95f19f9679678acc7121
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-12 00:13:35 +0530

    the program check no. of clockwise rotaions in an array

---
+++ c++/checkArrayRotaion.cpp
@@ -0,0 +1,35 @@
+/*A sorted array has been rotated by some number k in clockwise direction. Find
+k. E.g. Input: 5,6,1,2,3,4 Output: 2 */
+
+#include<iostream>
+using namespace std;
+
+int FindSortedArrayRotation(int arr[], int n){
+	int i;
+	int count=0;
+    for(i=0;i<n-1;i++){
+        if(arr[i]<=arr[i+1])
+            count++;
+        else {count++;
+                break;}
+    }
+    if(i==n-1)
+        count=0;
+    return count;
+}
+
+int main(){
+    int n;
+    cout<<"no. of elements : ";
+    cin>>n;
+
+    cout<<endl;
+    cout<<"Enter elements : ";
+    int i;
+    int *arr = new int[n];
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+
+    cout<<endl<<"NO. of rotations(value of k) : "<<FindSortedArrayRotation(arr,n);
+    return 0;
+}
---
+++ c++/rotationOfArray.cpp
@@ -0,0 +1,47 @@
+/*
+Write a function Rotate(ar[], nr, n) that rotates arr[] of size n by nr elements
+(towards right).
+Eg. Input : {1, 2, 3, 4, 5, 6, 7} n = 7 and d = 2
+Output : {3, 4, 5, 6, 7, 1, 2}
+*/
+
+#include<iostream>
+using namespace std;
+
+void Rotate(int arr[],int nr,int n)
+{
+    int i,j,k,prev,temp;
+    while (nr>0){
+        i=n-2;
+        prev=arr[n-1];
+        temp=arr[n-1];
+
+        for(i=n-2;i>=0;i--){
+            prev=temp;
+            temp=arr[i];
+            arr[i]=prev;
+        }
+        arr[n-1]=temp;
+    nr--;
+    }
+}
+int main(){
+//n=no.of elements
+//nr = no. of rotations
+    int n,nr;
+    cout<<"no. of elements and no. of rotations : ";
+    cin>>n>>nr;
+
+    cout<<endl;
+    cout<<"Enter elements : ";
+    int i;
+    int *arr = new int[n];
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+
+    Rotate(arr,nr,n);
+    cout<<endl;
+    for(i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+
+}
