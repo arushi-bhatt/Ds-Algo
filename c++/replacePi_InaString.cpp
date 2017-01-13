116c9bac880fbc854c0a4ac98c66c0dd41fd6dc0
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-13 23:18:00 +0530

    documentation added

--- c++/replacePi_InaString.cpp
+++ c++/replacePi_InaString.cpp
@@ -1,8 +1,36 @@
+/*
+Replace pi (recursive)
+Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+Sample Input 1 :
+
+xpix
+
+Sample Output :
+
+x3.14x
+
+Sample Input 2 :
+
+pipi
+
+Sample Output :
+
+3.143.14
+
+Sample Input 3 :
+
+pip
+
+Sample Output :
+
+3.14p
+*/
+
 #include<iostream>
 #include<cstdio>
 using namespace std;
 // Change in the given string itself. So no need to return or print anything
-
+//count keeps the record of total no of 'pi' till now
 void helper(char str[],int si,int count)
 {
     if(str[si]=='\0')
@@ -35,11 +63,11 @@ void replacePi(char input[]) {
 
 }
 
-int main() {
-char str[40];
-cout<<"enter string : ";
-gets(str);
-replacePi(str);
-cout<<str;
-
+int main(){
+    char str[40];
+    cout<<"enter string : ";
+    gets(str);
+    replacePi(str);
+    cout<<str;
+    return 0;
 }
