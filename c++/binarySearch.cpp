8ea555720e2fb089480855c0d239a88bd48a797c
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-01-14 21:34:07 +0530

    documentation added

--- c++/binarySearch.cpp
+++ c++/binarySearch.cpp
@@ -1,5 +1,27 @@
 //Binary Search (only for sorted array)
+/*
+Binary Search (Recursive)
+Given an integer sorted array (sorted in increasing order) and an element x, find the x in given array using binary search. Return the index of x.
+Return -1 if x is not present in the given array.
 
+Input format :
+
+Line 1 : Array size
+
+Line 2 : Array elements (separated by space)
+
+Line 3 : x (element to be searched)
+Sample Input :
+
+6
+2 3 4 5 6 8
+5
+
+Sample Output:
+
+3
+
+*/
 #include<iostream>
 using namespace std;
 
