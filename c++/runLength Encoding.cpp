54a3d3649f75db6aeb1d98ac4ea71d7050e7c95d
Arushi Bhatt <arushi131.bhatt@gmail.com>
2017-02-03 23:58:14 +0530

    doc added

--- c++/runLength Encoding.cpp
+++ c++/runLength Encoding.cpp
@@ -6,8 +6,7 @@
 #define MAX_RLEN 50
 using namespace std;
 
-char *encode(char *src)
-{
+char *encode(char *src){
   int rLen;
   char count[MAX_RLEN];
   int len = strlen(src);
@@ -17,16 +16,15 @@ char *encode(char *src)
   {
    dest[j++] = src[i];
     rLen = 1;
-    while(i + 1 < len && src[i] == src[i+1])
-    {
+    while(i + 1 < len && src[i] == src[i+1]){
       rLen++;
       i++;
     }
 
+
      sprintf(count, "%d", rLen);
 
-    for(k = 0; *(count+k); k++, j++)
-    {
+    for(k = 0; *(count+k); k++, j++){
       dest[j] = count[k];
     }
   }
@@ -57,9 +55,9 @@ int main(){
     cout<<"enter the string to be compressed  : ";
     gets(str);
     char *res=encode(str);
-    cout<<endl<<"\nEncoded text_____________"<<endl;
+    cout<<endl<<"\nEncoded text___________"<<endl;
     cout<<res<<endl;
-    cout<<endl<<"\nDecoded text_____________"<<endl;
+    cout<<endl<<"\nDecoded text___________"<<endl;
     res=decode(res);
     cout<<res<<endl;
     return 0;
